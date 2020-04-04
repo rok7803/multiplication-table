84bf4266e2863d04a550b6e34dd8d7337f6323f4
ha6icht <rolf.karlen@gmail.com>
2020-04-04 20:52:32 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,27 @@ import './App.css';
 import Settings from '../Settings/Settings';
 
 class App extends React.Component {
-  getSubmit(all, two){
+  getSubmit(two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, termTen, termTwelve, forward, random, backward){
     let series = [];
+    let term, direction;
     //console.log(all);
-    if(all !== 0) series.push(1);
-    if(two !== 0) series.push(2);
-    console.log(series);
+    if(two !== 0) series.push(two);
+    if(three !== 0) series.push(three);
+    if(four !== 0) series.push(four);
+    if(five !== 0) series.push(five);
+    if(six !== 0) series.push(six);
+    if(seven !== 0) series.push(seven);
+    if(eight !== 0) series.push(eight);
+    if(nine !== 0) series.push(nine);
+    if(ten !== 0) series.push(ten);
+    if(eleven !== 0) series.push(eleven);
+    if(twelve !== 0) series.push(twelve);
+    if(termTen !== 0) term = termTen;
+    if(termTwelve !== 0) term = termTwelve;
+    if(forward !== 0) direction = forward;
+    if(random !== 0) direction = random;
+    if(backward !== 0) direction = backward;
+    console.log('['+series+'] '+term+' '+direction);
   }
   render(){
     return (
