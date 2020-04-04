89adb943e3ffda7d708dcb1c1c36c9e4c73eb7f9
ha6icht <rolf.karlen@gmail.com>
2020-04-04 22:32:01 +0200

    Import Component SetTable

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Settings from '../Settings/Settings';
+import SetTable from '../SetTable/SetTable';
+
+const series = [2];
+const term = 10;
+const direction = 1;
 
 class App extends React.Component {
   getSubmit(two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, termTen, termTwelve, forward, random, backward){
     let series = [];
     let term, direction;
-    //console.log(all);
+
     if(two !== 0) series.push(two);
     if(three !== 0) series.push(three);
     if(four !== 0) series.push(four);
@@ -23,13 +28,18 @@ class App extends React.Component {
     if(forward !== 0) direction = forward;
     if(random !== 0) direction = random;
     if(backward !== 0) direction = backward;
-    console.log('['+series+'] '+term+' '+direction);
+
+    const checked = series.reduce((a,b) => a + b, 0);
+    if(checked === 54 || checked === 77) series.unshift(1);
+    
+    console.log('['+series+'] '+term+' '+direction+' '+checked);
   }
   render(){
     return (
       <div className="App">
         <h1>Multiplication Table</h1>
         <Settings getSubmit={this.getSubmit}/>
+        <SetTable series={series} term={term}/>
       </div>
     );
   }
