7761ee0fec2c44c382a088def0310d209d3c2631
ha6icht <rolf.karlen@gmail.com>
2020-04-05 21:38:36 +0200

    Add square

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import Settings from '../Settings/Settings';
 import SetTable from '../SetTable/SetTable';
 
-const series = [2];
+const series = [1,2,3,4,5,6,7,8,9,10];
 const term = 10;
 const direction = 1;
+const square = false;
 
 class App extends React.Component {
   getSubmit(two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, termTen, termTwelve, forward, random, backward){
     let series = [];
     let term, direction;
     let checked54 = true;
+    let square = false;
     if(two !== 0) series.push(two);
     if(three !== 0) series.push(three);
     if(four !== 0) series.push(four);
@@ -36,17 +38,23 @@ class App extends React.Component {
     if(backward !== 0) direction = backward;
 
     const checked = series.reduce((a,b) => a + b, 0);
-    if(checked === 54 && checked54 && termTen !== 0) series.unshift(1);
-    if(checked === 77 && termTwelve !== 0) series.unshift(1);
+    if(checked === 54 && checked54 && termTen !== 0){
+      series.unshift(1);
+      square = true;
+    }
+    if(checked === 77 && termTwelve !== 0){
+      series.unshift(1);
+      square = true;
+    }
 
-    console.log('['+series+'] '+term+' '+direction+' '+checked);
+    console.log('['+series+'] '+term+' '+direction+' '+checked+' '+checked54+' '+square);
   }
   render(){
     return (
       <div className="App">
         <h1>Multiplication Table</h1>
         <Settings getSubmit={this.getSubmit}/>
-        <SetTable series={series} term={term}/>
+        <SetTable series={series} term={term} square={square} />
       </div>
     );
   }
