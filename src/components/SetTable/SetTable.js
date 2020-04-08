5827924a310f13a9f364f3faf4211009cfaa60b3
ha6icht <rolf.karlen@gmail.com>
2020-04-08 09:39:08 +0200

    Update SetTable.js

--- src/components/SetTable/SetTable.js
+++ src/components/SetTable/SetTable.js
@@ -5,13 +5,16 @@ import Table from '../Table/Table';
 class SetTable extends React.Component{
     render(){
         return(
-            <table className="Table">
-                <tbody>
-                    {this.props.series.map(serie =>{
-                        return <Table key={serie} serie={serie} term={this.props.term} square={this.props.square} />;
-                    })}
-                </tbody>
-            </table>
+            <div className="Multiplication-table">
+                <table className="Table">
+                    <tbody>
+                        {this.props.series.map(serie =>{
+                            return <Table key={serie} serie={serie} term={this.props.term} square={this.props.square} />;
+                        })}
+                    </tbody>
+                </table>
+                
+            </div>
         )
     }
 }
