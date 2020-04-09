094a00edcfa7def66b88b8d2f77bbe6b403a8220
ha6icht <rolf.karlen@gmail.com>
2020-04-09 15:04:40 +0200

    Update setMultiplier()

--- src/components/SetExercise/SetExercise.js
+++ src/components/SetExercise/SetExercise.js
@@ -2,9 +2,6 @@ import React from 'react';
 import './SetExercise.css';
 import Exercise from '../Exercise/Exercise';
 
-
-//let multiplier = -1;
-
 class SetExercise extends React.Component{
     constructor(props){
         super(props);
@@ -15,56 +12,41 @@ class SetExercise extends React.Component{
             resultButton1: 0,
             resultButton2: 0,
             resultButton3: 0,
+            counter: 0,
+            countNumber: 1,
             counterBackward10: 10,
-            counterBackward12: 12
+            counterBackward12: 12,
+            timerOn: false,
+            timerStart: 0,
+            timerTime: 0,
+            startExercise: false
         }
-        this.setMultiplier1 = this.setMultiplier1.bind(this);
-        this.setMultiplier2 = this.setMultiplier2.bind(this);
+        this.setSerie = this.setSerie.bind(this);
+        this.setMultiplier = this.setMultiplier.bind(this);
         this.getResult = this.getResult.bind(this);
-        //this.getRandomButton = this.getRandomButton.bind(this);
-        //this.setButton1 = this.setButton1.bind(this);
-        //this.setButton2 = this.setButton2.bind(this);
-        //this.setButton3 = this.setButton3.bind(this);
-        //this.handleResultButton1 = this.handleResultButton1.bind(this);
-        //this.handleResultButton2 = this.handleResultButton2.bind(this);
-        //this.handleResultButton3 = this.handleResultButton3.bind(this);
-        //this.renderMultiplier1 = this.renderMultiplier1.bind(this);
-        //this.renderMultiplier2 = this.renderMultiplier2.bind(this);
+        this.startExercise = this.startExercise.bind(this);
         this.nextExercise = this.nextExercise.bind(this);
-        
         this.stopExercise = this.stopExercise.bind(this);
         this.resetExercise = this.resetExercise.bind(this);
+        this.startTimer = this.startTimer.bind(this);
+        this.stopTimer = this.stopTimer.bind(this);
+        //this.resetTimer = this.resetTimer.bind(this);
     }
     componentDidMount(){
-        //this.setMultiplier1();
-        //this.setMultiplier2();
         this.getResult();
-        //this.getRandomButton();
-        //this.setButton1();
-        //this.setButton2();
-        //this.setButton3();
-        //console.log(this.state.result);
-    }
-    componentDidUpdate(prevState){
-        //if(this.state.multiplier !== prevState.multiplier) this.getResult();
+        //console.log('In componentDidMount');
     }
     componentWillUnmount(){
-        //this.setMultiplier1();
-        //this.setMultiplier2();
-        //this.getRandomButton();
-        //this.setButton1();
-        //this.setButton2();
-        //this.setButton3();
+        //console.log('In componentWillUnmount');
     }
-    setMultiplier1(){
+    setSerie(){
         let serie;
         if(this.props.series.length > 1){
             let item = Math.floor(Math.random()*(this.props.series.length));
             serie = this.props.series[item];
-            this.setState({serie: serie}/*, () => {serie = this.state.serie}*/);
-            //console.log(this.state.serie);
+            this.setState({serie: serie});
+            //console.log('In ifthis.state.serie: ',this.state.serie);
         }
-        //else this.setState({serie: this.props.series[0]}/*, () => {serie = this.state.serie;}*/);
         else{
             serie = this.props.series[0];
             this.setState({serie: serie});
@@ -72,100 +54,145 @@ class SetExercise extends React.Component{
         //console.log('Serie: '+serie);
         return serie;
     }
-    setMultiplier2(){
-        let multiplier;
+    setMultiplier(){
+        let multiplier, counter, counterBackward10, counterBackward12;
         if(this.props.direction === 1 && this.state.multiplier < this.props.term){
             multiplier = this.state.multiplier + this.props.step;
             this.setState({multiplier: multiplier}/*, () => {multiplier = this.state.multiplier}*/);
         }
         else if(this.props.direction === 2 && this.props.term === 10 && this.state.counter < this.props.term){
-            this.setState({multiplier: Math.floor(Math.random()*10+1),
-                counter: this.state.counter + 1
+            multiplier = Math.floor(Math.random()*10+1);
+            counter = this.state.counter + 1;
+            this.setState({
+                multiplier: multiplier,
+                counter: counter
             });
         }
         else if(this.props.direction === 2 && this.props.term === 12 && this.state.counter < this.props.term){
-            this.setState({multiplier: Math.floor(Math.random()*12+1), 
-                counter: this.state.counter + 1
+            multiplier = Math.floor(Math.random()*12+1);
+            counter = this.state.counter + 1;
+            this.setState({
+                multiplier: multiplier, 
+                counter: counter
             });
         }
         else if(this.props.direction === 3 && this.props.term === 10 && this.state.counterBackward10 > 0){
-            this.setState({multiplier: this.state.counterBackward10,
-                counterBackward10: this.state.counterBackward10 - 1
+            multiplier = this.state.counterBackward10;
+            counterBackward10 = this.state.counterBackward10 -1 ;
+            this.setState({
+                multiplier: multiplier,
+                counterBackward10: counterBackward10
             });
         }
         else if(this.props.direction === 3 && this.props.term === 12 && this.state.counterBackward12 > 0){
-            this.setState({multiplier: this.state.counterBackward12,
-                counterBackward12: this.state.counterBackward12 - 1
+            multiplier = this.state.counterBackward12;
+            counterBackward12 = this.state.counterBackward12 - 1;
+            this.setState({multiplier: multiplier,
+                counterBackward12: counterBackward12
             });
         }
-        //console.log(this.state.multiplier);
+        //console.log('multiplier: '+multiplier);
         return multiplier;
     }
-    getResult(){
-        //const result = await this.multiplication.call(this);
-        //const newResult = await this.state.serie * this.state.multiplier;
-        /*const serie = this.state.serie;
-        const multiplier = this.state.multiplier;
-        const result = serie * multiplier;*/
-        //console.log(serie+'*'+multiplier+'='+result);
-        //let multiplier = 0;
-        const serie = this.setMultiplier1();
-        const multiplier = this.setMultiplier2();
+    getResult(){     
+        const serie = this.setSerie();
+        const multiplier = this.setMultiplier();
         const result = serie * multiplier;
-        console.log('serie: '+serie+' multiplier: '+multiplier);
-        this.setState({result: result},
-            () => {console.log('State updated', this.state);});
+        //console.log('serie: '+serie+' multiplier: '+multiplier+' result: '+result);
+        this.setState({
+            result: result,
+            countNumber: 1
+        }/*,
+            () => {console.log('State updated: ', this.state);}*/);
         this.getRandomButton(result)
-        //console.log('In getResult: '+this.state.result);
+        //console.log('In getResult(this.state.result): '+this.state.result);
     }
     getRandomButton(result){
-        /*this.setState({
-            buttonArray: [...this.state.buttonArray, Math.floor(Math.random() * 3)]
-        });
-        this.setState({
-            buttonArray: [...this.state.buttonArray, Math.floor(Math.random()*this.state.result+1)]
-        });
-        this.setState({
-            buttonArray: [...this.state.buttonArray, Math.floor(Math.random()*this.state.result+1)]*/
-        //}/*, () => console.log('this.state.buttonArray: '+this.state.buttonArray)*/);
-        //console.log('this.state.buttonArray: '+this.state.buttonArray);
-        //console.log('this.state.result: '+this.state.result);
         let buttonArray = [];
         buttonArray.push(Math.floor(Math.random() * 3));
         buttonArray.push(Math.floor(Math.random()*result+1));
         buttonArray.push(Math.floor(Math.random()*result+1));
-        //this.setState({buttonArray: newArray}, () => {console.log('this.state.buttonArray: '+this.state.buttonArray);});
-        //console.log('Inside getRandomButton()')
+        
+        //console.log('In getRandomButton')
         this.setButton1(buttonArray, result);
         this.setButton2(buttonArray, result);
         this.setButton3(buttonArray, result);
-        //return this.state.buttonArray;
+        
     }
-    async setButton1(buttonArray, result){
-        //console.log('result: '+result);
-        console.log(buttonArray);
-        //console.log('Inside setButton1');
-        if(buttonArray[0] === 0) await this.setState({resultButton1: result})
-        else if(buttonArray[0] === 1) await this.setState({resultButton1: result + buttonArray[1]});
-        else if(buttonArray[0] === 2) await this.setState({resultButton1: result - buttonArray[2]});
-        console.log(this.state.resultButton1);
-        console.log(this.state.resultButton2);
-        console.log(this.state.resultButton3);
+    setButton1(buttonArray, result){
+        let resultPlus, resultMinus;
+        //console.log('buttonArray: '+buttonArray);
+        //console.log('In setButton1');
+        if(buttonArray[0] === 0) this.setState({resultButton1: result});
+        else if(buttonArray[0] === 1){
+            resultPlus = result + buttonArray[1];
+            this.setState({resultButton1: resultPlus});
+        }
+        else if(buttonArray[0] === 2){
+            resultMinus = result - buttonArray[2];
+            this.setState({resultButton1: resultMinus});
+        }
+        //console.log('this.state.resultButton1: '+this.state.resultButton1);
+        //console.log(('this.state.resultButton2: '+this.state.resultButton2);
+        //console.log(('this.state.resultButton3: '+this.state.resultButton3);
     }
-    async setButton2(buttonArray, result){
-        if(buttonArray[0] === 1) await this.setState({resultButton2: result})
-        else if(buttonArray[0] === 2) await this.setState({resultButton2: result + buttonArray[1]});
-        else if(buttonArray[0] === 0) await this.setState({resultButton2: result - buttonArray[2]});
+    setButton2(buttonArray, result){
+        let resultPlus, resultMinus;
+        if(buttonArray[0] === 1) this.setState({resultButton2: result});
+        else if(buttonArray[0] === 2){
+            resultPlus = result + buttonArray[1];
+            this.setState({resultButton2: resultPlus});
+        }
+        else if(buttonArray[0] === 0){
+            resultMinus = result - buttonArray[2];
+            this.setState({resultButton2: resultMinus});
+        }
     }
-    async setButton3(buttonArray, result){
-        if(buttonArray[0] === 2) await this.setState({resultButton3: result})
-        else if(buttonArray[0] === 0) await this.setState({resultButton3: result + buttonArray[1]});
-        else if(buttonArray[0] === 1) await this.setState({resultButton3: result - buttonArray[2]});
+    setButton3(buttonArray, result){
+        let resultPlus, resultMinus;
+        if(buttonArray[0] === 2) this.setState({resultButton3: result})
+        else if(buttonArray[0] === 0){
+            resultPlus = result + buttonArray[1];
+            this.setState({resultButton3: resultPlus});
+        }
+        else if(buttonArray[0] === 1){
+            resultMinus = result - buttonArray[2];
+            this.setState({resultButton3: resultMinus});
+        }
+    }
+    startExercise(){
+        //if(this.state.startExercise){
+            /*this.setState({
+                multiplier: 0,
+                startExercise: true
+            }, () => {
+                console.log('Callback this.setState (this.state.multiplier): ', this.state.multiplier);
+                /*this.getResult();*///});
+            this.startTimer();
+        /*}
+        else{
+            this.setState({
+                multiplier: -1,
+                startExercise: false
+            }, () => {this.getResult();});
+        }*/
+        console.log('In startExercise (this.state.multiplier): ', this.state.multiplier);
     }
     nextExercise(){
         this.getResult();
     }
+    stopExercise(){
+        this.stopTimer();
+        const { timerTime } = this.state;
+        let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+        let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+        let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+        //let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
+        alert('You did it! Your time: '+minutes+':'+seconds+':'+centiseconds);
+        this.resetExercise();
+    }
     resetExercise(){
+        this.stopTimer();
         this.setState({
             multiplier: 0,
             serie: 0,
@@ -173,29 +200,60 @@ class SetExercise extends React.Component{
             resultButton1: 0,
             resultButton2: 0,
             resultButton3: 0,
+            counter: 0,
+            countNumber: 0,
             counterBackward10: 10,
-            counterBackward12: 12
-        }, () => {this.getResult();});    
+            counterBackward12: 12,
+            timerStart: 0,
+            timerTime: 0
+        }, () => {this.getResult();});       
     }
-    stopExercise(){
-        alert('You did it!');
-        this.resetExercise(this);
+    startTimer(){
+        this.setState({
+            timerOn: true,
+            timerTime: this.state.timerTime,
+            timerStart: Date.now() - this.state.timerTime
+        });
+        this.timer = setInterval(() => {
+            this.setState({
+                timerTime: Date.now() - this.state.timerStart
+            });
+        }, 10);
     }
+    stopTimer(){
+        this.setState({timerOn: false});
+        clearInterval(this.timer);
+    }
+    /*resetTimer(){
+        this.setState({
+            timerStart: 0,
+            timerTime: 0
+        });
+    }*/
     render(){
         return(
-            <table className="Exercise-table">
-                <Exercise 
-                    serie={this.state.serie} 
-                    multiplier={this.state.multiplier}
-                    result={this.state.result}
-                    button1={this.state.resultButton1}
-                    button2={this.state.resultButton2}
-                    button3={this.state.resultButton3}
-                    nextExercise={this.nextExercise}
-                    stopExercise={this.stopExercise}
-                    term={this.props.term}
-                    />
-            </table>
+            <div className="Exercise-div">
+                <table className="Exercise-table">
+                    <Exercise 
+                        serie={this.state.serie} 
+                        multiplier={this.state.multiplier}
+                        result={this.state.result}
+                        button1={this.state.resultButton1}
+                        button2={this.state.resultButton2}
+                        button3={this.state.resultButton3}
+                        startExercise={this.startExercise}
+                        nextExercise={this.nextExercise}
+                        stopExercise={this.stopExercise}
+                        term={this.props.term}
+                        count={this.state.countNumber}
+                        reset={this.resetCountNumber}
+                        />
+                </table>
+                <div className="Exercise-button">
+                    <button onClick={this.resetExercise}>Reset</button>
+                    <button onClick={this.props.settingsOn}>Show Settings</button>
+                </div>
+            </div>
         );
     }
 }
