f6df56aa4179208ab8ce44811d556285b9c4e310
ha6icht <rolf.karlen@gmail.com>
2021-12-10 22:09:59 +0100

    Change some files

---
+++ build/precache-manifest.3bd3d486d5046619bab29e78af1bdbf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af161d16609911052fb85b049084cd6d",
+    "url": "/multiplication-table/index.html"
+  },
+  {
+    "revision": "276c144edae46911eded",
+    "url": "/multiplication-table/static/css/main.cb5dbda7.chunk.css"
+  },
+  {
+    "revision": "6a23fb001f0da9ffd722",
+    "url": "/multiplication-table/static/js/2.1b8e9901.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/multiplication-table/static/js/2.1b8e9901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "276c144edae46911eded",
+    "url": "/multiplication-table/static/js/main.139773f8.chunk.js"
+  },
+  {
+    "revision": "6b3c19ddcfe567b61c87",
+    "url": "/multiplication-table/static/js/runtime-main.41e8a727.js"
+  }
+]);
