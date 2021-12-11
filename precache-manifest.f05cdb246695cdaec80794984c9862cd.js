8dd331319eb6efaceabb2da01bf79ffbc06c043b
ha6icht <rolf.karlen@gmail.com>
2021-12-11 17:15:02 +0100

    Updates

---
+++ precache-manifest.f05cdb246695cdaec80794984c9862cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e639263723b650736139df72e04d91",
+    "url": "/multiplication-table/index.html"
+  },
+  {
+    "revision": "86b6f8cdf8d8335ef2ca",
+    "url": "/multiplication-table/static/css/main.4c19110d.chunk.css"
+  },
+  {
+    "revision": "6a23fb001f0da9ffd722",
+    "url": "/multiplication-table/static/js/2.1b8e9901.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/multiplication-table/static/js/2.1b8e9901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b6f8cdf8d8335ef2ca",
+    "url": "/multiplication-table/static/js/main.3d3fbbba.chunk.js"
+  },
+  {
+    "revision": "6b3c19ddcfe567b61c87",
+    "url": "/multiplication-table/static/js/runtime-main.41e8a727.js"
+  }
+]);
