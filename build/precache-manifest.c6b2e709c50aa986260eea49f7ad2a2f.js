ea3b7319f3fa6300e3eade99e87f35e572d8a303
ha6icht <rolf.karlen@gmail.com>
2022-03-22 19:35:21 +0100

    First commit

---
+++ build/precache-manifest.c6b2e709c50aa986260eea49f7ad2a2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67fcd8c4c6d4baa7924e92b0f200787",
+    "url": "/multiplication-table/index.html"
+  },
+  {
+    "revision": "01ebb5f286ff685afaff",
+    "url": "/multiplication-table/static/css/main.d04f6090.chunk.css"
+  },
+  {
+    "revision": "6a23fb001f0da9ffd722",
+    "url": "/multiplication-table/static/js/2.1b8e9901.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/multiplication-table/static/js/2.1b8e9901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ebb5f286ff685afaff",
+    "url": "/multiplication-table/static/js/main.3d3fbbba.chunk.js"
+  },
+  {
+    "revision": "6b3c19ddcfe567b61c87",
+    "url": "/multiplication-table/static/js/runtime-main.41e8a727.js"
+  }
+]);
